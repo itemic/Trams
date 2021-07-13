5e493230a648d3e81490f92ba291c17808afc31e
Terran Kroft <terrankroft@gmail.com>
2021-07-13 18:45:05 +1000

    Initial Commit

---
+++ Trams/TramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TramsApp.swift
+//  Trams
+//
+//  Created by Terran Kroft on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
